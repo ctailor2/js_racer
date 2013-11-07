2818a8577deab9ae6be44301ea26458ce399a2ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-07 15:24:33 -0600

    refactoring js to oo in progress

--- public/js/driver.js
+++ public/js/driver.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-  $("#start_race").submit(function(event){
-    var p1 = new Player($("#player1").val());
-    var p2 = new Player($("#player2").val());
-    var race = new Race(p1, p2);
-  });
+  var p1 = new Player($("#player1_strip").data("player-id"));
+  var p2 = new Player($("#player2_strip").data("player-id"));
+  var race = new Race(p1, p2);
 
-  debugger;
+  race.keyListener()
+
 });
 
--- public/js/game.js
+++ public/js/game.js
@@ -1,33 +1,89 @@
-$(document).ready(function() {
-  var player1_position = 1;
-  var player2_position = 1;
-  $(document).on('keyup', function(event) {
-    event.preventDefault();
+// $(document).ready(function() {
+//   var player1_position = 1;
+//   var player2_position = 1;
+//   $(document).on('keyup', function(event) {
+//     event.preventDefault();
+//     var key = event.keyCode;
+//     if (key == 81){
+//       var current_block = $("#player1_strip .active");
+//       var next_block = current_block.next();
+//       current_block.removeClass();
+//       next_block.addClass("active");
+//       player1_position += 1
+//       if (player1_position == 20) {
+//         alert("Player 1 wins!");
+//         $(document).off("keyup");
+//         location.reload();
+//       }
+//     }
+//     else if(key == 80){
+//       var current_block = $("#player2_strip .active");
+//       var next_block = current_block.next();
+//       current_block.removeClass();
+//       next_block.addClass("active");
+//       player2_position += 1
+//       if (player2_position == 20) {
+//         alert("Player 2 wins!");
+//         $(document).off("keyup");
+//         location.reload();
+//       }
+//     }
+//   });
+
+// });
+
+function Race(p1, p2){
+  this.p1 = p1;
+  this.p2 = p2;
+}
+
+Race.prototype.keyListener = function(){
+  var race = this;
+
+  $(document).on("keyup", function(event){
     var key = event.keyCode;
     if (key == 81){
-      var current_block = $("#player1_strip .active");
-      var next_block = current_block.next();
-      current_block.removeClass();
-      next_block.addClass("active");
-      player1_position += 1
-      if (player1_position == 20) {
-        alert("Player 1 wins!");
-        $(document).off("keyup");
-        location.reload();
-      }
+      race.updatePosition(race.p1)
     }
-    else if(key == 80){
-      var current_block = $("#player2_strip .active");
-      var next_block = current_block.next();
-      current_block.removeClass();
-      next_block.addClass("active");
-      player2_position += 1
-      if (player2_position == 20) {
-        alert("Player 2 wins!");
-        $(document).off("keyup");
-        location.reload();
-      }
+    else if (key == 80){
+      race.updatePosition(race.p2)
     }
   });
+}
 
-});
+Race.prototype.start = function(){
+  race = this;
+  $(document).one("keyup", function(event){
+    race.startTime = event.
+  })
+}
+
+Race.prototype.updatePosition = function(player){
+  var currentBlock = $("#player" + player.id.toString() + "_strip .active")
+  var nextBlock = currentBlock.next();
+  currentBlock.removeClass();
+  nextBlock.addClass("active");
+  player.advance();
+  this.winChecker()
+}
+
+Race.prototype.winChecker = function(){
+  if this.p1.position == 20 {
+    this.annouceWinner(p1);
+  }
+  else if this.p2.position == 20 {
+    this.annouceWinner(p2);
+  }
+}
+
+
+
+Race.prototype.announceWinner = function(player){
+  $.post("/record_results", { 
+    duration: duration, 
+    winner_id: $("#player1_strip").data("player-id"), 
+    race_id: $(".racer_table").data("race-id") }, 
+    function(response){
+      $("#results").html(response);
+    });
+}
