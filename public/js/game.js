f73c79099dd8dc6aced6528d659f7477d7485b52
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-07 16:29:11 -0600

    deleted unused js file, updated announceWinner to off the keyup event

--- public/js/game.js
+++ public/js/game.js
@@ -1,37 +1,3 @@
-// $(document).ready(function() {
-//   var player1_position = 1;
-//   var player2_position = 1;
-//   $(document).on('keyup', function(event) {
-//     event.preventDefault();
-//     var key = event.keyCode;
-//     if (key == 81){
-//       var current_block = $("#player1_strip .active");
-//       var next_block = current_block.next();
-//       current_block.removeClass();
-//       next_block.addClass("active");
-//       player1_position += 1
-//       if (player1_position == 20) {
-//         alert("Player 1 wins!");
-//         $(document).off("keyup");
-//         location.reload();
-//       }
-//     }
-//     else if(key == 80){
-//       var current_block = $("#player2_strip .active");
-//       var next_block = current_block.next();
-//       current_block.removeClass();
-//       next_block.addClass("active");
-//       player2_position += 1
-//       if (player2_position == 20) {
-//         alert("Player 2 wins!");
-//         $(document).off("keyup");
-//         location.reload();
-//       }
-//     }
-//   });
-
-// });
-
 function Race(p1, p2){
   this.p1 = p1;
   this.p2 = p2;
@@ -81,6 +47,7 @@ Race.prototype.winChecker = function(){
 
 Race.prototype.announceWinner = function(player){
   race = this;
+  $(document).off("keyup");
   $.post("/record_results", { 
     duration: ($.now() - race.startTime)/1000.0, 
     winner_id: player.id, 
